ec83a2fe2199394cf5d9f984b3eda426cdd5bd29
luupaloop <meraflake.d.melow@gmail.com>
2023-03-13 23:48:04 +0300

    Initial commit

---
+++ src/20050151001.c
@@ -0,0 +1,8 @@
+#include <20050151001.h>
+
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
+}
---
+++ src/hellofunc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <20050151001.h>
+
+void myPrintHelloMake(void) {
+
+  printf("Hello makefiles!\n");
+
+  return;
+}
